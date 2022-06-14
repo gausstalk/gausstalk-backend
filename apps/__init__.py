72e79873e36671ff4c4cddb48c5b42f2fda3a575
YoohaBae <yooha.bae.2@gmail.com>
2022-06-14 16:12:32 +0900

    add registration of one on ones

--- apps/__init__.py
+++ apps/__init__.py
@@ -5,7 +5,9 @@ router prefix is /apps
 from fastapi import APIRouter
 from .chat import router as chat_router
 from .user import router as user_router
+from .meeting import router as meeting_router
 
 router = APIRouter()
 router.include_router(chat_router, prefix='/chat')
 router.include_router(user_router, prefix='/user')
+router.include_router(meeting_router, prefix="/meeting")
---
+++ apps/meeting/endpoints/v1/meeting.py
@@ -0,0 +1,71 @@
+'''
+Path functions for /apps/meeting
+'''
+from typing import List
+
+import datetime
+
+from fastapi import status, APIRouter, Depends, Response
+from fastapi.responses import JSONResponse
+
+from services.mongo_service import get_mongo
+from apps.user.services.auth_service import auth_user
+
+from apps.chat.models.message import Message
+from apps.user.models import auth
+
+router = APIRouter()
+
+
+@router.get(
+    '/',
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {'model': Message},
+        status.HTTP_404_NOT_FOUND: {'model': Message},
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {'model': Message}
+    },
+    dependencies=[Depends(auth_user)],
+)
+def get_register_exists(
+        user: auth.User = Depends(auth_user),
+        database=Depends(get_mongo)
+):
+    """ Check if meeting registration exists """
+    today = datetime.date.today()
+    if database.meetings.find_one({'mail': user['mail'], 'date': today.strftime("%Y-%m-%d")}):
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={'message': 'Register exists'}
+        )
+
+    return JSONResponse(
+        status_code=status.HTTP_204_NO_CONTENT
+    )
+
+
+@router.put(
+    '/',
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {'model': Message},
+        status.HTTP_404_NOT_FOUND: {'model': Message},
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {'model': Message}
+    },
+    dependencies=[Depends(auth_user)],
+)
+def put_register(
+        user: auth.User = Depends(auth_user),
+        database=Depends(get_mongo)
+):
+    """ Add registration to db """
+    today = datetime.date.today()
+
+    try:
+        database.meetings.insert_one({'mail': user['mail'], 'date': today.strftime("%Y-%m-%d")})
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={'message': 'Register completed'}
+        )
+    except:
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
