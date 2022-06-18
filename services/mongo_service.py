52a943540e2c534e0022f011e9aa20f517d01bfa
Choi Yeonung <yonsweng@gmail.com>
2022-06-18 18:49:49 +0900

    automate sending emails

--- services/mongo_service.py
+++ services/mongo_service.py
@@ -10,6 +10,6 @@ mongo_client = MongoClient(os.getenv('MONGO_URI'))
 mongo_db = mongo_client[os.getenv('MONGO_DB')]  # dev/prd
 
 
-async def get_mongo():
+def get_mongo():
     ''' Global variable for MongoDB instance. '''
     return mongo_db
