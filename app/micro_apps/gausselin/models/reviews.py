1b06622d485033ef7cf199fa3d08f3d5d6088ed8
Choi Yeonung <yonsweng@gmail.com>
2022-07-23 23:34:50 +0900

    upgrade gausselin reviews APIs based on the frontend

--- app/micro_apps/gausselin/models/reviews.py
+++ app/micro_apps/gausselin/models/reviews.py
@@ -4,15 +4,19 @@ Model for /apps/gausselin/v1/reviews.
 
 # pylint: disable=no-name-in-module, too-few-public-methods
 
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, constr
 
 
 class ReviewRequest(BaseModel):
     """
     Model for gausselin review request.
     """
+    restaurant_id: int
+    restaurant_name: str | None
     stars: int
-    comment: str
+    comment: constr(max_length=280) | None
 
 
 class ReviewResponse(ReviewRequest):
@@ -21,3 +25,4 @@ class ReviewResponse(ReviewRequest):
     """
     id: str
     user_mail: str
+    user_name: Optional[str]
